ece0e6ddff2520eea1c538853b96141efb05adc0
Luis <gomesluis.dev@gmail.com>
2025-09-12 11:33:01 -0300

    firs

---
+++ main.js
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from "electron";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: join(__dirname, "preloader.js"),
+    },
+  });
+  win.loadURL("http://localhost:5173/");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform != "darwin") app.quit();
+});
